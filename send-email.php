ab5628e7b96f6e4596a16db31bfdaf62e1366599
Luis Alcala <lalcalaroblero2@gmail.com>
2025-12-28 22:44:49 -0600

    Add files via upload
    in addition, you will need composer from this: https://github.com/PHPMailer/PHPMailer?tab=readme-ov-file

---
+++ send-email.php
@@ -0,0 +1,15 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+$mail-> isSMTP();
+$mail ->SMTPAuth = true;
